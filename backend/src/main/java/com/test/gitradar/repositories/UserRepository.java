1f948fffe38b90ee6006455cc97e7f031faefd24
DastinovDS <daniil.stepanov@digitalgedacht.de>
2026-04-15 16:33:36 +0200

    #7 Updated TestController to check PostgreSQL, created Repo/UserRepository interfaces, added getters/setters/default constructors for models(due to problems with Lombok auto-setters/getters). Changed device port from 5432 to 5433(due to issues with local PostgreSQL process which takes 5432 port)

---
+++ backend/src/main/java/com/test/gitradar/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.test.gitradar.repositories;
+
+import com.test.gitradar.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
